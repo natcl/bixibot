ceedc699cab1042fdff317f8064415ba83614e78
Nathanaël Lécaudé <nat@studioimaginaire.com>
2014-06-24 01:28:11 +0000

    Better handling of locked stations

--- webpy/bixibot.py
+++ webpy/bixibot.py
@@ -9,6 +9,8 @@ import json
 import random
 import os
 
+numStations = 15
+
 urls = (
     "/", "index",
     "/station", "station"
@@ -27,17 +29,26 @@ class station:
         
 def init_station():
     if not os.path.exists('bixi.json'):
-        tableau = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+        tableau = [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
         index_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
         bikes, stations = get_station_info('Saint-Laurent / Guizot')
+        lockedStations = numStations - (bikes + stations)
+
         for x in range(bikes):
             index = random.choice(index_list)
             index_list.remove(index)
             tableau[index] = 1
+        
         for x in range(stations):
             index = random.choice(index_list)
             index_list.remove(index)
             tableau[index] = 0
+        
+        for x in range(lockedStations):
+            index = random.choice(index_list)
+            index_list.remove(index)
+            tableau[index] = 2
+
         with open('bixi.json', 'w') as f:
             f.write(json.dumps(tableau))
         return json.dumps(tableau)
@@ -60,25 +71,54 @@ def update_station():
                 locked_index.append(i)
 
         bikes, stations = get_station_info('Saint-Laurent / Guizot')
+        lockedStations = numStations - (bikes + stations)
+        
         previous_bikes = len(bikes_index)
         previous_stations = len(stations_index)
+        previous_locked = len(locked_index)
         
-        if previous_stations == stations and previous_bikes == bikes:
+        if previous_stations == stations and previous_bikes == bikes and previous_locked == lockedStations:
             return json.dumps(previous_state)
+
+        if lockedStations > previous_locked:
+            locked_diff = lockedStations - previous_locked
+            for x in range(locked_diff):
+                try:
+                    i = random.choice(stations_index)
+                    previous_state[i] = 2
+                    stations_index.remove(i)
+                except IndexError:
+                    break
+
+        if lockedStations < previous_locked:
+            locked_diff = previous_locked - lockedStations
+            for x in range(locked_diff):
+                try:
+                    i = random.choice(locked_index)
+                    previous_state[i] = 0
+                    locked_index.remove(i)
+                except IndexError:
+                    break
 
         if bikes > previous_bikes:
             bike_diff = bikes -  previous_bikes
             for x in range(bike_diff):
-                i = random.choice(stations_index)
-                previous_state[i] = 1
-                stations_index.remove(i)
+                try:
+                    i = random.choice(stations_index)
+                    previous_state[i] = 1
+                    stations_index.remove(i)
+                except IndexError:
+                    break
 
         if stations > previous_stations:
             station_diff = stations - previous_stations
             for x in range(station_diff):
-                i = random.choice(bikes_index)
-                previous_state[i] = 0
-                bikes_index.remove(i)
+                try:
+                    i = random.choice(bikes_index)
+                    previous_state[i] = 0
+                    bikes_index.remove(i)
+                except IndexError:
+                    break
 
         with open('bixi.json', 'w') as f:
             f.write(json.dumps(previous_state))
@@ -116,6 +156,7 @@ def get_station_info(query):
                         found = True
                         result = (int(nbBikes), int(nbEmptyDocks))
         if (found):
+            #return (5,10)
             return result
         else:
             return 'Station inexistante'
